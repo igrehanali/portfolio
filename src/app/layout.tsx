e35d0c69bae5a5ebb789a7702a25f4129fd5551e
igrehanali <muhammadrehanali031@gmail.com>
2024-11-06 23:34:34 +0500

    host

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,9 @@ const calistoga = Calistoga({
   variable: "--font-serif",
   weight: "400",
 });
+
 export const metadata: Metadata = {
-  title: "Rehan's Web Development Portfolio ",
+  title: "Rehan's Web Development Portfolio",
   description:
     "Explore Rehan's professional portfolio featuring innovative web development projects crafted with Next.js, React, and Tailwind CSS. Discover his expertise in creating high-performance, responsive, and modern applications.",
 };
@@ -22,6 +23,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:5198285,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+      </head>
       <body
         className={twMerge(
           inter.variable,
